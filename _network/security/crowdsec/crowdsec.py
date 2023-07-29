d94f9c2c7d7a558f10d9a2c61e0a461cf4d9b6ae
Vo Van Nghia <git@vnghia.com>
2023-07-29 11:00:33 +0200

    crowdsec: enable online api

--- _network/security/crowdsec/crowdsec.py
+++ _network/security/crowdsec/crowdsec.py
@@ -4,9 +4,12 @@ import pulumi_random as random
 from pulumi import ComponentResource, ResourceOptions
 
 from _command import Command
+from _common import get_logical_name, service_config
 from _container import DockerContainer
 from _file import Template
 from _network.resource import child_opts
+
+_crowdsec_config = service_config["crowdsec"]
 
 
 # TODO: Connection Central API does not work in bridge network.
@@ -19,16 +22,24 @@ class Crowdsec(ComponentResource):
         self.__acquis_traefik = self.__template.build(
             module_path=Path(__file__).parent / "acquis" / "traefik.py"
         )
+        self.__online_api = self.__template.build(
+            module_path=Path(__file__).parent / "online_api.py"
+        )
         self.__container = DockerContainer.build(
             "crowdsec",
             opts=self.__child_opts,
             envs={
                 "COLLECTIONS": "crowdsecurity/traefik",
                 "CUSTOM_HOSTNAME": "crowdsec",
-                "DISABLE_ONLINE_API": "true",
+                "ENROLL_KEY": _crowdsec_config["console-id"],
+                "ENROLL_INSTANCE_NAME": get_logical_name(),
+                "USE_WAL": "true",
             },
             volumes={"/var/run/docker.sock": {"ro": True}},
-            labels={"acquis-traefik-sha256": self.__acquis_traefik["sha256"]},
+            labels={
+                "acquis-traefik-sha256": self.__acquis_traefik["sha256"],
+                "online-api-sha256": self.__online_api["sha256"],
+            },
         )
         self.container_id = self.__container.id
         self.register_outputs({"container_id": self.container_id})
