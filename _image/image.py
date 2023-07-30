42d5efeeadace87ec8e3e92d86bbe5ca5d00e766
Vo Van Nghia <git@vnghia.com>
2023-07-31 00:48:10 +0200

    docker(image): add docker build image

--- _image/image.py
+++ _image/image.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
+import pulumi_docker as docker
 from pulumi import ComponentResource, ResourceOptions
 
 from _command import Command
-from _common import docker_config, server_config
+from _common import constant, docker_config, get_logical_name, server_config
 
 
 class DockerImage(ComponentResource):
@@ -19,6 +20,8 @@ class DockerImage(ComponentResource):
             self.image_map[key] = self.__get_image_id(
                 name=data["name"], tag=data["tag"], platform=platform
             )
+
+        self.__build_image_build(platform)
 
     def __get_image_id(self, name: str, tag: str, platform: str):
         tag_ = tag
@@ -42,6 +45,39 @@ class DockerImage(ComponentResource):
                 },
             ).stdout,
         }
+
+    def __build_image_build(self, platform: str):
+        build_config = docker_config.get("build", {})
+        for key, data in build_config.items():
+            context_path = (
+                Path(data["context"])
+                .resolve(True)
+                .relative_to(constant.PROJECT_ROOT_DIR)
+            )
+            dockerfile = (
+                (context_path / data.get("dockerfile", "Dockerfile"))
+                .resolve(True)
+                .relative_to(constant.PROJECT_ROOT_DIR)
+            )
+
+            image_full_name = "{}/{}:{}".format(
+                constant.PROJECT_NAME, key, constant.PROJECT_STACK
+            )
+            self.image_map[key] = {
+                "image_name": image_full_name,
+                "image_id": docker.Image(
+                    get_logical_name(key),
+                    build=docker.DockerBuildArgs(
+                        args=data.get("args"),
+                        builder_version=docker.BuilderVersion.BUILDER_BUILD_KIT,
+                        context=str(context_path),
+                        dockerfile=str(dockerfile),
+                        platform="linux/{}".format(platform),
+                    ),
+                    image_name=image_full_name,
+                    skip_push=True,
+                ).id,
+            }
 
 
 docker_image = DockerImage()
