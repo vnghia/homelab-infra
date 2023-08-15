5f44db83b470e9ba6c3c7cf2ee977cf6f383be79
Vo Van Nghia <git@vnghia.com>
2023-08-15 23:32:55 +0200

    network(bridge): use constant name

--- _network/docker/bridge.py
+++ _network/docker/bridge.py
@@ -1,11 +1,13 @@
 import pulumi_docker as docker
+from pulumi import ResourceOptions
 
 from _common import constant, get_logical_name
 from _network.resource import child_opts
 
 default_bridge_network = docker.Network(
     get_logical_name("bridge"),
-    opts=child_opts,
+    opts=child_opts.merge(ResourceOptions(delete_before_replace=True)),
+    name=get_logical_name("bridge"),
     driver="bridge",
     labels=[
         docker.NetworkLabelArgs(label=k, value=v)
