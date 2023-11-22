f6e2d08291e789f3131ca3944f9cbb85697bcb97
Vo Van Nghia <git@vnghia.com>
2023-11-22 17:50:21 +0100

    service(ntfy): fix typo in database path

--- _service/ntfy/server.py
+++ _service/ntfy/server.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from _common import container_storage_config, service_config
 from _network.dns import hostnames
 
@@ -5,7 +8,7 @@ _ntfy_config = service_config["ntfy"]
 _ntfy_volume = container_storage_config["ntfy"]
 
 NTFY_SMTP_PORT = 25
-NTFY_SMTP_DOMAIN = hostnames[_ntfy_config["email"]["domain"]]
+NTFY_SMTP_DOMAIN = hostnames[_ntfy_config["email-server"]["domain"]]
 
 output_config = {
     "type": "yaml",
@@ -14,9 +17,9 @@ output_config = {
     "path": "server.yml",
     "input": {
         "base-url": "https://{}".format(hostnames["public-ntfy"]),
-        "cache-file": "{}cache.db".format(_ntfy_volume["cache"]["volume"]),
+        "cache-file": os.fspath(Path(_ntfy_volume["cache"]["dir"]) / "cache.db"),
         "cache-startup-queries": "pragma journal_mode = WAL;\npragma synchronous = normal;\npragma temp_store = memory;\npragma busy_timeout = 15000;\nvacuum;\n",
-        "auth-file": "{}user.db".format(_ntfy_volume["data"]["volume"]),
+        "auth-file": os.fspath(Path(_ntfy_volume["data"]["dir"]) / "user.db"),
         "auth-default-access": "deny-all",
         "auth-startup-queries": "pragma journal_mode = WAL;\npragma synchronous = normal;\npragma temp_store = memory;\npragma busy_timeout = 15000;\nvacuum;\n",
         "smtp-server-listen": ":{}".format(NTFY_SMTP_PORT),
