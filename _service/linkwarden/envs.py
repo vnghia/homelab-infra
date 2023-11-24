bf33c86c94d40360ed7db0d54e345c631e9d9350
Vo Van Nghia <git@vnghia.com>
2023-11-24 03:09:46 +0100

    service(linkwarden): move archives to s3

--- _service/linkwarden/envs.py
+++ _service/linkwarden/envs.py
@@ -1,5 +1,6 @@
 from pulumi import Output
 
+from _common import storage_config
 from _data.postgres import postgres
 from _network.dns.hostnames import hostnames
 from _secret import secret
@@ -18,4 +19,9 @@ envs = {
     ),
     "NEXTAUTH_URL": "https://{}".format(hostnames["private-linkwarden"]),
     "NEXT_PUBLIC_DISABLE_REGISTRATION": True,
+    "SPACES_KEY": storage_config["key-id"],
+    "SPACES_SECRET": storage_config["key-secret"],
+    "SPACES_ENDPOINT": storage_config["endpoint"],
+    "SPACES_REGION": storage_config["region"],
+    "BUCKET_NAME": storage_config["bucket"],
 }
