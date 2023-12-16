4f44261e144e2b02a4c42b08dc8101e78c33f994
Vo Van Nghia <git@vnghia.com>
2023-12-16 14:20:16 +0100

    service(navidrome): use env instead of config file

---
+++ _service/navidrome/envs.py
@@ -0,0 +1,18 @@
+from _common import container_storage_config, service_config
+from _secret import secret
+
+_navidrome_config = service_config["navidrome"]
+_navidrome_volume = container_storage_config["navidrome"]
+
+envs = {
+    "ND_MUSICFOLDER": _navidrome_volume["music"]["dir"],
+    "ND_DATAFOLDER": _navidrome_volume["data"]["dir"],
+    "ND_LOGLEVEL": "info",
+    "ND_PASSWORDENCRYPTIONKEY": secret.keys["navidrome-encryption"].result,
+    "ND_SCANSCHEDULE": "@every 24h",
+    "ND_SPOTIFY_ID": _navidrome_config["spotify"]["id"],
+    "ND_SPOTIFY_SECRET": _navidrome_config["spotify"]["secret"],
+    "ND_SUBSONICARTISTPARTICIPATIONS": True,
+    "ND_TRANSCODINGCACHESIZE": "1GB",
+    "ND_ENABLESHARING": True,
+}
--- _service/navidrome/navidrome.py
+++ _service/navidrome/navidrome.py
@@ -1,39 +1,23 @@
-from pathlib import Path
-
 from pulumi import ComponentResource, ResourceOptions
 
-from _common import container_storage_config
 from _container import DockerContainer
-from _file import Template
 from _network.traefik import traefik_proxy
+from _service.navidrome.envs import envs
 from _service.resource import child_opts
-
-_navidrome_volume = container_storage_config["navidrome"]
 
 
 class Navidrome(ComponentResource):
     def __init__(self) -> None:
         super().__init__("service:index:Navidrome", "navidrome", None, child_opts)
-        self.__child_opts = ResourceOptions(
-            parent=self, depends_on=[traefik_proxy.dynamic_config["navidrome"]["file"]]
-        )
-
-        self.__navidrome_config = Template(self.__child_opts).build(
-            module_path=Path(__file__).parent / "config.py"
-        )
         self.__container = DockerContainer.build(
             "navidrome",
-            opts=self.__child_opts,
-            envs={
-                "ND_CONFIGFILE": "{}{}".format(
-                    _navidrome_volume["data"]["dir"],
-                    self.__navidrome_config["config"]["path"],
-                )
-            },
-            labels={"navidrome-config-sha256": self.__navidrome_config["sha256"]},
+            opts=ResourceOptions(
+                parent=self,
+                depends_on=[traefik_proxy.dynamic_config["navidrome"]["file"]],
+            ),
+            envs=envs,
             wait=True,
         )
-
         self.container_id = self.__container.id
         self.register_outputs({"container_id": self.container_id})
 
