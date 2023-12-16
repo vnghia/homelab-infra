bbfcd31e9cfec9a53f22beab16ab3eb83e2352bf
Vo Van Nghia <git@vnghia.com>
2023-12-16 01:00:22 +0100

    chore: upgrade immich to v1.91.0

--- _service/immich/envs.py
+++ _service/immich/envs.py
@@ -1,12 +1,9 @@
-from _common import container_storage_config
 from _data.postgres import postgres
 from _data.redis import redis
 from _network.dns import hostnames
-from _secret import secret
 
 _redis_config = redis.db["immich"]
 _postgres_config = postgres.db["immich"]
-_immich_typesense_volume = container_storage_config["immich-typesense"]
 
 envs = {
     "PUBLIC_IMMICH_SERVER_URL": "https://{}".format(hostnames["private-immich"]),
@@ -19,11 +16,4 @@ envs = {
     "REDIS_PORT": _redis_config["port"],
     "REDIS_USERNAME": _redis_config["username"],
     "REDIS_PASSWORD": _redis_config["password"],
-    "TYPESENSE_API_KEY": secret.build_string("typesense-api-key", length=32).result,
-    "TYPESENSE_HOST": "immich-typesense",
-}
-
-typesense_envs = {
-    "TYPESENSE_API_KEY": envs["TYPESENSE_API_KEY"],
-    "TYPESENSE_DATA_DIR": _immich_typesense_volume["data"]["dir"],
 }
--- _service/immich/immich.py
+++ _service/immich/immich.py
@@ -2,7 +2,7 @@ from pulumi import ComponentResource, ResourceOptions
 
 from _container import DockerContainer
 from _network.traefik import traefik_proxy
-from _service.immich.envs import envs, typesense_envs
+from _service.immich.envs import envs
 from _service.resource import child_opts
 
 
@@ -33,20 +33,15 @@ class Immich(ComponentResource):
             opts=self.__child_opts,
             envs=envs | {"MACHINE_LEARNING_MODEL_TTL": 3600},
         )
-        self.__typesense_container = DockerContainer.build(
-            "immich-typesense", opts=self.__child_opts, envs=typesense_envs
-        )
 
         self.server_container_id = self.__server_container.id
         self.microservices_container_id = self.__microservices_container.id
         self.machine_learning_container_id = self.__machine_learning_container.id
-        self.typesense_container_id = self.__typesense_container.id
         self.register_outputs(
             {
                 "server_container_id": self.server_container_id,
                 "microservices_container_id": self.microservices_container_id,
                 "machine_learning_container_id": self.machine_learning_container_id,
-                "typesense_container_id": self.typesense_container_id,
             }
         )
 
