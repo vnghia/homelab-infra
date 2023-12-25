ef2be10f543cbfed67646f17638dc15c861cee16
Vo Van Nghia <git@vnghia.com>
2023-12-25 13:26:17 +0700

    service(script-server): add build and run docker net

--- _service/script_server/docker.py
+++ _service/script_server/docker.py
@@ -1,4 +1,5 @@
 from _common import constant, container_storage_config, get_logical_name
+from _network.docker import default_bridge_network
 
 _script_server_volume = container_storage_config["script-server"]
 
@@ -25,15 +26,25 @@ for k, v in constant.PROJECT_TAG.items():
     docker_label += ["-l", "{}={}".format(k, v)]
 
 
-def build_run_net_tailscale(name: str | None = None):
+def __build_run_net(name: str | None = None, network: str | None = None):
     return (
         [
             "docker",
             "run",
             "--rm",
             "--network",
-            "container:{}".format(get_logical_name("tailscale")),
+            network or default_bridge_network.name,
         ]
         + (["--name", get_logical_name(name)] if name else [])
         + docker_label
     )
+
+
+def build_run_net_tailscale(name: str | None = None):
+    return __build_run_net(
+        name=name, network="container:{}".format(get_logical_name("tailscale"))
+    )
+
+
+def build_run_net_bridge(name: str | None = None):
+    return __build_run_net(name)
