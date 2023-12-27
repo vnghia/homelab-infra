094235f5fce95a45f51072386f29cd53449b0d6a
Vo Van Nghia <git@vnghia.com>
2023-12-28 00:35:24 +0700

    common(constant): allow set project_name from env

--- _common/constant.py
+++ _common/constant.py
@@ -1,10 +1,11 @@
+import os
 from pathlib import Path
 
 import pulumi
 
 PROJECT_ROOT_DIR = Path.cwd()
 
-PROJECT_NAME = pulumi.get_project()
+PROJECT_NAME = os.environ.get("PULUMI_PROJECT_NAME", pulumi.get_project())
 PROJECT_STACK = pulumi.get_stack()
 PROJECT_TAG = {
     "pulumi": "1",
