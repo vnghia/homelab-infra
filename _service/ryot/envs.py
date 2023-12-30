abf15f03f729995919b0aac53902a5faba5797a7
Vo Van Nghia <git@vnghia.com>
2023-12-30 23:04:36 +0700

    service(ryot): use database connection string

--- _service/ryot/envs.py
+++ _service/ryot/envs.py
@@ -1,19 +1,10 @@
-from pulumi import Output
-
 from _common import storage_config
 from _data.postgres import postgres
 
 _postgres_config = postgres.db["ryot"]
 
 envs = {
-    "DATABASE_URL": Output.format(
-        "postgresql://{0}:{1}@{2}:{3}/{4}",
-        _postgres_config["username"],
-        _postgres_config["password"],
-        _postgres_config["host"],
-        _postgres_config["port"],
-        _postgres_config["database"],
-    ),
+    "DATABASE_URL": _postgres_config["url"],
     "FILE_STORAGE_S3_ACCESS_KEY_ID": storage_config["key-id"],
     "FILE_STORAGE_S3_SECRET_ACCESS_KEY": storage_config["key-secret"],
     "FILE_STORAGE_S3_URL": storage_config["endpoint"],
