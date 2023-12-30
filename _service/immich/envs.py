e6b27c6e619f12ca883071d09d5579860524ecd7
Vo Van Nghia <git@vnghia.com>
2023-12-30 22:51:27 +0700

    service(immich): use connection string

--- _service/immich/envs.py
+++ _service/immich/envs.py
@@ -7,11 +7,7 @@ _postgres_config = postgres.db["immich"]
 
 envs = {
     "PUBLIC_IMMICH_SERVER_URL": "https://{}".format(hostnames["private-immich"]),
-    "DB_HOSTNAME": _postgres_config["host"],
-    "DB_PORT": _postgres_config["port"],
-    "DB_USERNAME": _postgres_config["username"],
-    "DB_PASSWORD": _postgres_config["password"],
-    "DB_DATABASE_NAME": _postgres_config["database"],
+    "DB_URL": _postgres_config["url"],
     "REDIS_HOSTNAME": _redis_config["host"],
     "REDIS_PORT": _redis_config["port"],
     "REDIS_USERNAME": _redis_config["username"],
