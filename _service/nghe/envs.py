32bd64845cad75041e4833c72425cc7a50c1ebb4
Vo Van Nghia <git@vnghia.com>
2024-04-25 22:47:20 +0200

    chore: upgrade nghe to 0.8.0

--- _service/nghe/envs.py
+++ _service/nghe/envs.py
@@ -1,21 +1,20 @@
 import binascii
 
-from _common import container_storage_config
+from _common import container_storage_config, service_config
 from _data.postgres import postgres
 from _secret import secret
 
+_nghe_config = service_config["nghe"]
 _nghe_volume = container_storage_config["nghe"]
 _postgres_config = postgres.db["nghe"]
 
 envs = {
-    "NGHE_SERVER__HOST": "::",
     "NGHE_SERVER__PORT": 3000,
     "NGHE_DATABASE__KEY": secret.keys["nghe-encryption"].result.apply(
         lambda x: binascii.hexlify(x.encode()).decode("ascii")
     ),
     "NGHE_DATABASE__URL": _postgres_config["url"],
-    "NGHE_FOLDER__TOP_PATHS": str([_nghe_volume["music-self"]["dir"]]).replace(
-        "'", '"'
-    ),
-    "RUST_LOG": "nghe=debug,tower_http=debug",
+    "NGHE_LASTFM__KEY": _nghe_config["lastfm"]["key"],
+    "NGHE_SPOTIFY__ID": _nghe_config["spotify"]["id"],
+    "NGHE_SPOTIFY__SECRET": _nghe_config["spotify"]["secret"],
 }
